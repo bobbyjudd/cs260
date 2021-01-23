e31966702aa2c8c4ecb6b47a97a4b9655f302dbd
Bobby Judd <bobbymjudd@gmail.com>
2021-01-23 10:46:44 -0800

    Implement simple logistic regression

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -4,7 +4,27 @@ batch_size = 64
 from torch.utils.data.sampler import SubsetRandomSampler
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchvision import datasets, transforms
+
+class LogisticRegression(nn.Module):
+    def __init__(self):
+        super(LogisticRegression, self).__init__()
+        self.linear = torch.nn.Linear(28**2, 1)
+
+    def forward(self, x):
+        y_pred = torch.sigmoid(self.linear(x))
+        return y_pred
+
+def accuracy(pred, labels):
+    correct = (pred.view(-1).long() == labels).sum()
+    print(pred.view(-1).long())
+    print(labels)
+    total = pred.size(0)
+    return 100 * (correct.float() / total)
+
+#class LinearSVM(nn.Module):
+
 
 
 ## USE THIS SNIPPET TO GET BINARY TRAIN/TEST DATA
@@ -30,20 +50,37 @@ test_loader = torch.utils.data.DataLoader(test_data,batch_size=batch_size,
   shuffle=False,sampler=SubsetRandomSampler(subset_indices.view(-1)))
 
 # The number of epochs is at least 10, you can increase it to achieve better performance
-num_epochs = 10
+num_epochs = 100
+
+loss_func = nn.BCELoss()
+model = LogisticRegression()
+opt = torch.optim.SGD(model.parameters(), lr=0.01)
 
 # Training the Model
 for epoch in range(num_epochs):
     total_loss = 0
+    correct, total = 0., 0.
     for i, (images, labels) in enumerate(train_loader):
         images = images.view(-1, 28*28)
+
         #Convert labels from 0,1 to -1,1
-        labels = 2*(labels.float()-0.5)
+        current_batch_size = labels.size(0)
+        #labels = (2*(labels.float()-0.5)).long()
+        reshaped_labels = labels.float().reshape(current_batch_size, 1)
+
+        pred = model(images)
+        loss = loss_func(pred, reshaped_labels)
+        loss.backward()
+        opt.step()
+
+        total_loss += loss.item()
+        temp_corr = (pred.view(-1).long() == labels).sum()
+        correct += temp_corr
+        total += images.shape[0]
         
-        ## TODO 
-
+    epoch_accuracy = 100 * (correct.float() / total)
     ## Print your results every epoch
-
+    print("Epoch {}: Loss {} Accuracy {}".format(epoch, total_loss, epoch_accuracy))
 
 # Test the Model
 correct = 0.
@@ -52,7 +89,7 @@ for images, labels in test_loader:
     images = images.view(-1, 28*28)
     
     ## Put your prediction code here, currently use a random prediction
-    prediction = torch.randint(0,2,labels.shape)
+    prediction = model(images)
 
     correct += (prediction.view(-1).long() == labels).sum()
     total += images.shape[0]
